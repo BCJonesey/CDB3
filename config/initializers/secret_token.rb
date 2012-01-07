a5dd4fb63d88d56e28300e8a4a686ae5a57344ce
Nat Tuck <nat@ferrus.net>
2012-01-07 15:32:13 -0500

    Stock rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdb3::Application.config.secret_token = 'a1b307ba6ae6fbde5833c9ba6252a221f52dd2efedc0598a6a74dd573601dba28d8f081092f9ae69d2c1e1122535688fe4fbf46225bc18f577fed2b97c82c7bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cdb3::Application.config.session_store :cookie_store, key: '_cdb3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cdb3::Application.config.session_store :active_record_store

aebfc8a155f7b36ac5cd1fe01849468c3c51fb68
Nat Tuck <nat@ferrus.net>
2012-01-07 23:38:18 -0500

    List games; test.

--- test/functional/games_controller_test.rb
+++ test/functional/games_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "shoud include MM" do
     get :index
     assert_response :success
+    assert(@response.body.include?(games(:mirror_mirror).name), "No MM name")
+
   end
 end

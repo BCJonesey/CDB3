0c340b8b5a279c3fe77e7df61b1c0a7439830ab1
Nat Tuck <nat@ferrus.net>
2012-01-07 20:20:14 -0500

    Add test: successful login.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,4 @@
+
 class MainController < ApplicationController
   def index
   end
@@ -6,8 +7,11 @@ class MainController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     if @user.nil?
+      session[:user_id] = nil
       redirect_to '/', :alert => "No such user"
       return
+    else
+      session[:user_id] = @user.id
     end
   end
 

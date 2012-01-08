85a491e5a44ed33aba8cb8cbd1a36545cd3f888e
Ben Jones <ben@cerberusentertainment.com>
2012-01-08 00:33:29 -0500

    ensure that email is unique and does not contain uppercase letters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :members
+  validates :name, :presence => true
+  validates :email, :presence => true,:uniqueness=>true,:format => {:with => /^[^A-Z]+$/} 
+  before_validation :lower_email
+  
+  private 
+  def lower_email
+    self.email = self.email.downcase
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
    test "check user name" do
        assert "Darren Gagne" == users(:darren).name
    end
+   
+   test "prevent uppercase" do
+     user = User.create({"email"=>"sssSS@g.com"})
+   end
+   
+   test "ensure uniqe" do 
+     user = User.create({"email"=>users(:darren).email})
+     assert_not_equal user.errors.count , 0
+   end
 end

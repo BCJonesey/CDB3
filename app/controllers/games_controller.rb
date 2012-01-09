22197f8b6b2debcb99544b503d8b17f5848ca763
Nat Tuck <nat@ferrus.net>
2012-01-09 00:01:39 -0500

    REST actions in games controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,21 +7,36 @@ class GamesController < ApplicationController
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.create(params[:game])
+    redirect_to game_path(@game), :notice => "Game created: #{@game.name}"
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+    @game.update_attributes(params[:game])
+    redirect_to game_path(@game), :notice => "Game updated: #{@game.name}"    
   end
 
   def destroy
+    @game = Game.find(params[:id])
+    if (params[:confirm] == "Yes")
+      @game.destroy
+      redirect_to games_path, :notice => "Game deleted: #{@game.name}"
+    else
+      redirect_to game_path(@game), :alert => "Must confirm game deletion"
+    end
   end
 
 end
--- test/functional/games_controller_test.rb
+++ test/functional/games_controller_test.rb
@@ -25,4 +25,43 @@ class GamesControllerTest < ActionController::TestCase
     assert_redirected_to '/'
     assert_equal flash[:alert], "Access denied: requires global admin"   
   end
+
+  test "should display edit form" do
+    get :edit, {:id => games(:mirror_mirror).id}, {:user_id => users(:nat).id}
+    assert_response :success
+    assert(@response.body.include?("<form"), "No form")
+  end
+
+  test "should display mirror mirror" do
+    get :show, {:id => games(:mirror_mirror).id}, {:user_id => users(:nat).id}
+    assert_response :success
+    assert(@response.body.include?(games(:mirror_mirror).name), "No MM name")
+  end
+
+  test "should create new game" do
+    post :create, {:game => {:name => "Created Game"}}, {:user_id => users(:nat).id}
+    assert_response :redirect
+    assert_not_nil Game.find_by_name("Created Game")
+  end
+
+  test "should update game" do
+    post :update, {:id => games(:the_calling).id, 
+        :game => {:id => games(:the_calling).id, :name => "Teh Colling"}},
+      {:user_id => users(:nat).id}
+    game = Game.find_by_name("Teh Colling")
+    assert_equal game.id, games(:the_calling).id
+  end
+
+  test "should not destroy game without confirm" do 
+    post :destroy, {:id => games(:the_calling).id}, {:user_id => users(:nat).id}
+    assert_response :redirect
+    assert_equal flash[:alert], "Must confirm game deletion"
+    assert_not_nil Game.find_by_name("The Calling")    
+  end
+
+  test "should destroy game" do
+    post :destroy, {:id => games(:the_calling).id, :confirm => "Yes"}, {:user_id => users(:nat).id}
+    assert_response :redirect
+    assert_nil Game.find_by_name("The Calling")    
+  end
 end

52ea3efeb08ccc2fd197a8ae4dd9f766ed1ff37c
Nat Tuck <nat@ferrus.net>
2012-03-10 15:28:32 -0500

    Add some FactoryGirl tests.

---
+++ test/factories/users.rb
@@ -0,0 +1,8 @@
+
+FactoryGirl.define do
+  factory :user do
+    name "Goonbag McDouchepants"
+    email "goonbag@example.com"
+    global_admin false
+  end
+end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,26 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-   test "check user name" do
-       assert "Darren Gagne" == users(:darren).name
-   end
-   
-   test "prevent uppercase" do
-     user = User.create({"email"=>"sssSS@g.com"})
-   end
-   
-   test "ensure uniqe" do 
-     user = User.create({"email"=>users(:darren).email})
-     assert_not_equal user.errors.count , 0
-   end
-   
-   test "prevent delete of user when it has members" do
-     assert users(:darren).members.count >0
-     exception = assert_raises RuntimeError do
+  test "check user name" do
+    darren = FactoryGirl.build(:user, :name => "Darren")
+    assert darren.name == "Darren"
+  end
+  
+  test "prevent uppercase" do
+    user = FactoryGirl.create(:user, :email => "sssSS@example.com")
+    assert user.email == "sssss@example.com"
+  end
+  
+  test "ensure unique" do 
+    user = User.create({"email"=>users(:darren).email})
+    assert_not_equal user.errors.count, 0
+  end
+  
+  test "prevent delete of user when it has members" do
+    assert users(:darren).members.count >0
+    exception = assert_raises RuntimeError do
       users(:darren).destroy
-     end
-     assert_equal "Cannot delete a user with members", exception.message
-   end
+    end
+    assert_equal "Cannot delete a user with members", exception.message
+  end
 end
 
 

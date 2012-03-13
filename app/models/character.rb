8f9f6cafbfd04299ba49bc40a57070fadd883143
Ben Jones <ben@cerberusentertainment.com>
2012-03-13 15:33:46 -0400

    A character's first version is now only created when requested

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,17 +2,36 @@ class Character < ActiveRecord::Base
   belongs_to :member
   has_one :game, :through => :member
   belongs_to :character_version, :dependent => :destroy
-  has_many :character_skills, :through => :character_version
-  
+  delegate :character_skills, :to=> :character_version
   validates :name, :presence => true
-  validates :character_version_id, :presence => true
 
-  before_validation :check_version
   
-  def check_version
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #funky shit!
+  alias :real_character_version :character_version
+  def character_version
     if character_version_id.nil?
-      vv = CharacterVersion.create
-      self.character_version_id = vv
+      self.character_version = CharacterVersion.create
+      self.save
     end
+    self.real_character_version
   end
+  
 end

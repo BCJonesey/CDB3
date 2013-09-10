f09e72189b1b541e1d3604c69aafa857b447c299
Ben Jones <Ben@CerberusEntertainment.com>
2013-09-10 11:24:56 -0400

    require that player cap be set for an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :game
   has_many   :registrations
 
-  validates_presence_of :game_id, :site, :start_date, :end_date
+  validates_presence_of :game_id, :site, :start_date, :end_date, :player_cap
  
   scope :upcoming, lambda { where("end_date >= ?", Time.zone.now ) }
   scope :past, lambda { where("end_date < ?", Time.zone.now ) }

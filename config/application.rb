da8c77ae102806ddc976cc8ab86b8f685c35576e
Ben Jones <ben@cerberusentertainment.com>
2014-03-04 21:14:08 -0500

    attempt 5

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module CDB3
     config.action_controller.permit_all_parameters = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     config.assets.precompile += %w( .svg .eot .woff .ttf)
+    config.assets.precompile += %w( character_edit.js )
     console do
         require "pry"
         config.console = Pry

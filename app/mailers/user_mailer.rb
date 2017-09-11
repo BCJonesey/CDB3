d5b85a12d2c6bce5af47e7f4b55c5aa3dc5e8bbe
Ben Jones <ben@benjones.io>
2017-09-11 15:15:12 -0700

    slightly different email text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(:to => user.email, :subject => "Welcome to the LARP Tool")
+    mail(:to => user.email, :subject => "Welcome to LARP.ninja")
   end
 end

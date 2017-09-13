05e25e61641eeec588a1c36d968a7092beb81ccf
Ben Jones <ben@benjones.io>
2017-09-13 15:18:58 -0700

    proc yo

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,10 +6,10 @@ class Registration < ActiveRecord::Base
   validates_uniqueness_of :event_id,:scope => :member_id
   
   
-  scope :pcs, where(:cp_game_id =>nil)
-  scope :npcs, where("cp_game_id IS NOT NULL")
-  scope :prereged, where(:prereged=>true)
-  scope :present, where(:present=>true)
+  scope :pcs, -> {where(:cp_game_id =>nil)}
+  scope :npcs, -> {where("cp_game_id IS NOT NULL")}
+  scope :prereged, -> {where(:prereged=>true)}
+  scope :present, -> {where(:present=>true)}
   
   def waitlist_spot
     event.registrations.pcs.where("created_at < ?", created_at).count - event.player_cap

20a217ed2885c10f3bef1b998de2912c89c30a6e
Ben Jones <ben@benjones.io>
2017-09-13 22:55:22 -0700

    all good for now

--- app/assets/javascripts/angular/custom/app.js
+++
@@ -1,8 +0,0 @@
-var chareditApp = angular.module('chareditApp', [  
-	'chareditControllers', 'restangular']).
-  config(function(RestangularProvider) {
-
-
-RestangularProvider.setBaseUrl(skillsUrl);
-
-});

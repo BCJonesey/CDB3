75479a1920306f512ab9db117db34fe30825e5d7
Ben Jones <ben@benjones.io>
2017-09-20 16:38:54 -0700

    js nonsense!

---
+++ app/assets/javascripts/appStrap.js
@@ -0,0 +1,3 @@
+//= require appStrap/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js
+//= require appStrap/plugins/jPanelMenu/jquery.jpanelmenu.min.js
+//= require appStrap/script.js

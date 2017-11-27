cf6885be44413f38f8f6d6e549e22b3af3d82eb0
Ben Jones <ben@benjones.io>
2017-11-27 12:00:42 -0800

    shitty assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+
+Rails.application.config.action_controller.permit_all_parameters = true
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.sass.load_paths << File.expand_path('../../lib/assets/stylesheets/')
+Rails.application.config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf character_sheets/* appStrap.js)
+Rails.application.config.assets.precompile += %w( appStrap/plugins/jPanelMenu/jquery.jpanelmenu.min.js appStrap/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js)
+

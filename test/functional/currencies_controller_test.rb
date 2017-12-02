a4b127dbb8738d75b2464f830214f0d3bd9f792c
Ben Jones <ben@benjones.io>
2017-12-01 19:44:27 -0800

    more tests

--- test/functional/currencies_controller_test.rb
+++ test/functional/currencies_controller_test.rb
@@ -58,5 +58,52 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
         user_session.assert_redirected_to game_currencies_path(game)
       end
     end
+    describe "game non-admin" do
+      let(:is_game_admin){false}
+      setup do
+        user_session.get game_member_path(game, member)
+      end
+      test "should get index" do
+        user_session.get game_currencies_path(game)
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should get new" do
+        user_session.get new_game_currency_path(game)
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should create currency" do
+        assert_difference('Currency.count', 0) do
+          user_session.post game_currencies_path(game), params: {currency: FactoryBot.attributes_for(:currency)}
+        end
+
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should show currency" do
+        user_session.get game_currency_path(game, currency)
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should get edit" do
+        user_session.get edit_game_currency_path(game, currency)
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should update currency" do
+        user_session.put game_currency_path(game,currency), params: {currency: {short_name: FactoryBot.attributes_for(:currency)[:short_name]}}
+        user_session.assert_redirected_to game_path(game)
+      end
+
+      test "should destroy currency" do
+        user_session.get game_currency_path(game, currency)
+        assert_difference('Currency.count', 0) do
+          user_session.delete game_currency_path(game,currency)
+        end
+
+        user_session.assert_redirected_to game_path(game)
+      end
+    end
   end
 end

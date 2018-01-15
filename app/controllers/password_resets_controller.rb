01b92d1e6423c05f2c294848a1d36d5d377e9cfb
Ben Jones <ben@benjones.io>
2018-01-15 15:35:41 -0800

    make the forms look less bad

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -36,7 +36,7 @@ class PasswordResetsController < ApplicationController
     end
 
     # the next line makes the password confirmation validation work
-    @user.password_confirmation = params[:user][:password_confirmation]
+    # @user.password_confirmation = params[:user][:password_confirmation]
     # the next line clears the temporary token and updates the password
     if @user.change_password!(params[:user][:password])
       redirect_to(root_path, :notice => 'Password was successfully updated.')

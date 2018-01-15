dc6e698abd2887e0a56b9fd978aa59c1f9aa29f7
Ben Jones <ben@benjones.io>
2018-01-15 12:31:02 -0800

    password reset start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
